b0ab8db76ccf4382723cedcafd0d7e932a4861e7
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-04-28 04:01:58 -0700

    committed server side of Social Media App

---
+++ Routes/UserRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { UnFollowUser, deleteUser, followUser, getAllUsers, getUser, updateUser } from "../Controllers/UserController.js";
+import authMiddleWare from "../MiddleWare/authMiddleWare.js";
+
+
+const router = express.Router();
+
+router.get('/', getAllUsers);
+router.get('/:id', getUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+router.put('/:id/follow', followUser);
+router.put('/:id/unfollow', UnFollowUser);
+
+export default router;
+
